42398aeebc8d329374c04200a8a2fe04e35bda09
Elzy <elzyproot@protonmail.com>
2026-01-12 22:01:19 +0700

    Update vital files list in test_core_files_existence

--- tests/test_pr.py
+++ tests/test_pr.py
@@ -4,7 +4,8 @@ import importlib
 def test_core_files_existence():
     # Memastikan file vital tidak dihapus atau dipindah sembarangan
     vital_files = ["main.py", "requirements.txt", "app/__init__.py", "install.sh",
-                   "app/utility/utils.py", "app/utility/colors.py", "app/base/update.py", "search.py", "config_path.py", "banner.py",
+                   "app/utility/utils.py", "app/utility/colors.py", "app/base/update.py", 
+                   "app/utility/search.py", "app/utility/config_path.py", "app/banners/banner.py",
                    "tests/test_pr.py"]
     for f in vital_files:
         assert os.path.exists(f), f"File vital {f} HILANG! Jangan dihapus."
