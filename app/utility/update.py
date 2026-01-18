0f8fb5d2d1819dc7b66d35c0c42225c459dd0c9f
Elzy <elzyproot@protonmail.com>
2026-01-19 00:29:46 +0700

    Modify update command and clean up output
    Updated the update command URL and removed print statements.

--- app/utility/update.py
+++ app/utility/update.py
@@ -4,12 +4,8 @@ import sys
 from app.utility.colors import C
 
 def run_update():
-    print(f"{C.SUCCESS} [+] Start automatic updates...")
-
     os.chdir(os.path.expanduser("~"))
-    update_command = "curl -fsSL https://raw.githubusercontent.com/Proot9/Cyber-Pentest/main/install.sh | bash "
+    update_command = "curl -fsSL https://raw.githubusercontent.com/storm-os/Cyber-Pentest/main/install.sh | bash "
     os.system(update_command)
-
-    print(f"{C.SUCCESS} [+] The update is complete. Please run your tools again.")
     sys.exit()
 
