e2050132b85c265e826b8afccc8a4a649cac2f5d
Elzy <elzyproot@protonmail.com>
2026-01-22 13:57:35 +0700

    Update sip_scan.py

--- modules/auxiliary/voip/sip_scan.py
+++ modules/auxiliary/voip/sip_scan.py
@@ -24,7 +24,7 @@ def execute(options):
     )
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(10)
+    sock.settimeout(3)
     try:
         sock.sendto(payload.encode(), (ip, port))
         data, addr = sock.recvfrom(2048)
