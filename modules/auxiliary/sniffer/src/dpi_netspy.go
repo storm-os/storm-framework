e9d3097b0740f32864557fd4b649c61cdd5bbfef
Proot9 <elzyproot@protonmail.com>
2026-01-23 15:48:04 +0700

    add: modules sniffer low level

---
+++ modules/auxiliary/sniffer/src/dpi_netspy.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	// Checks whether interface arguments are given
+	if len(os.Args) < 2 {
+		fmt.Println("[!] Penggunaan: ./sniffer <interface>")
+		os.Exit(1)
+	}
+
+	device := os.Args[1] // Takes the interface from the first argument
+
+	// Opening the device for sniffing
+	handle, err := pcap.OpenLive(device, 1024, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatalf("[!] Failed to open device %s: %v", device, err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	fmt.Printf("[*] Storm Go-Sniffer active di %s...\n", device)
+
+	for packet := range packetSource.Packets() {
+		if appLayer := packet.ApplicationLayer(); appLayer != nil {
+			payload := string(appLayer.Payloadd())
+
+			// Simple payload analysis for credentials
+			keywords := []string{"user", "pass", "login", "pwd", "auth"}
+			for _, key := range keywords {
+				if strings.Contains(strings.ToLower(payload), key) {
+					fmt.Printf("\n[!] DATA DETECTED IN %s:\n%s\n", device, payload)
+					break
+				}
+			}
+		}
+	}
+}
