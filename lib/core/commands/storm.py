4348562b418118518ff8ced58ad5c03899569e84
Elzy <elzyproot@protonmail.com>
2026-02-05 05:47:55 +0700

    Fix import statement for run_update in storm.py

--- lib/core/commands/storm.py
+++ lib/core/commands/storm.py
@@ -1,4 +1,5 @@
-8from app.utility.update import run_update
+from app.utility.update import run_update
+import app.utility.colors from C
 
 def execute(args, context):
     cmd = args[0].lower() if args else ""
