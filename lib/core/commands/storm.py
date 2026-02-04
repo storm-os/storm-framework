197a15aff0f3cd236f859a23aa8d2b49908a89a9
Elzy <elzyproot@protonmail.com>
2026-02-05 05:49:06 +0700

    Fix import statement for colors in storm.py

--- lib/core/commands/storm.py
+++ lib/core/commands/storm.py
@@ -1,5 +1,5 @@
 from app.utility.update import run_update
-import app.utility.colors from C
+from app.utility.colors import C
 
 def execute(args, context):
     cmd = args[0].lower() if args else ""
