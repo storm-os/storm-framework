8b714f0d86f48a679dc95f6b0ebf1231fa88bbe4
Elzy <elzyproot@protonmail.com>
2026-02-08 23:06:27 +0700

    Update test_verify_integrity.py

--- tests/test_verify_integrity.py
+++ tests/test_verify_integrity.py
@@ -27,7 +27,9 @@ def test_verify_repo_integrity():
 
     # 3. Scan folder now
     current_manifest = {}
-    ignored = {'.git', '__pycache__', '.pytest_cache', '.github', 'database', 'gitignore'}
+    ignored = {'.git', '__pycache__', '.pytest_cache',
+               '.github', 'storm.db', '.gitignore',
+               'database'}
     
     for path in root_dir.rglob('*'):
         if path.is_file() and not any(p in path.parts for p in ignored):
