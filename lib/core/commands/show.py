1faaee2da133cd3a09f2e2d6b8331cadb36733f2
Elzy🐧 <elzyproot@protonmail.com>
2026-02-16 05:09:56 +0700

    Update show.py

--- lib/core/commands/show.py
+++ lib/core/commands/show.py
@@ -39,27 +39,7 @@ def execute(args, context):
                 print(f"{k:<12} {val:<25} Global Variable")
         print("")
 
-    # 3. show db (Gudang Data)
-    elif target_show == "db":
-        # Cek apakah ada argumen setelah 'db'
-        # Contoh: 'show db CVE-2017-0144' -> args[0] adalah CVE-ID nya
-        if len(args) > 1:
-            target_id = args[1]
-
-            # Jika argumennya adalah 'cve', tampilkan katalog (semua)
-            if target_id.lower() == "cve":
-                utils.cve_summary()
-            else:
-                # Jika argumennya adalah ID CVE (misal: CVE-2017-0144)
-                # Tampilkan detail satu per satu
-                utils.detail_cve(target_id)
-        else:
-            # Jika cuma ketik 'show db' tanpa argumen
-            print(
-                f"{C.INPUT}[*] Penggunaan: show db cve (untuk list) atau show db <CVE-ID> (untuk detail)"
-            )
-
-    # 4. show <category_name>
+    # 3. show <category_name>
     else:
         module_files = utils.get_modules_in_category(target_show)
         if module_files:
@@ -68,6 +48,6 @@ def execute(args, context):
                 print(f"  - {mod}")
             print("")
         else:
-            print(f"{C.ERROR}[-] Category or option '{target_show}' not found.")
+            print(f"{C.ERROR}[-] Category or option: {target_show} > not found.")
 
     return context
