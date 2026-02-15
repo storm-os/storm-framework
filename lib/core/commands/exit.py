7ca92a8b2367681c2eb91ae9cd6f433e4ff7af87
Elzy🐧 <elzyproot@protonmail.com>
2026-02-16 04:25:50 +0700

    Add comments clearly

--- lib/core/commands/exit.py
+++ lib/core/commands/exit.py
@@ -1,3 +1,7 @@
+# Exit command to avoid errors or crashes in storm.
+# Because if you only use CTRL + C it is possible that the storm will come out messy.
+# This will minimize the possibility of a crash to prevent damage.
+
 def execute(args, context):
     context["exit"] = True
     return context
