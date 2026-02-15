1afd688bc17f2f66418c0fac6b741845d12399f7
Elzy🐧 <elzyproot@protonmail.com>
2026-02-16 06:39:14 +0700

    Create ldch.py

---
+++ lib/smf/ldch.py
@@ -0,0 +1,28 @@
+import os
+
+def session(options, cache_folder="cache"):
+    cache_path = os.path.join(cache_folder, ".storm-session")
+
+    # check cache files
+    if os.path.exists(cache_path):
+        try:
+            with open(cache_path, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    if '=' in line:
+                        # Split based on the first '=' sign found
+                        key, value = line.split('=', 1)
+                        
+                        # fill the variable according to the cache content
+                        if key in options:
+                            options[key] = value
+            
+            # delete cache file
+            os.remove(cache_path)
+            return options
+        except Exception as e:
+            print(f"[!] Error loading session: {e}")
+            return options
+            
+    return options
+  
