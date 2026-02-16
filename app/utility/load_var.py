724a771df7f574eafb32e03ba2cb6efece15fdb5
Elzy🐧 <elzyproot@protonmail.com>
2026-02-16 07:18:53 +0700

    Create load_var.py

---
+++ app/utility/load_var.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import lib.smf.svch as svch
+
+def load_var():
+    # save old variables
+    svch.session(context["options"])
+
+    # Restart the storm
+    executable = sys.argv[0]
+    args = sys.argv
+    try:
+        os.execv(executable, args)
+    except Exception as e:
+        print(f"[-] Restart failed: {e}")
+        sys.exit(1)
+      
