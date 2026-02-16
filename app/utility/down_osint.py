c8c5d2ed7d995ef0e8cdf7eb44c5279578dd9807
Elzy🐧 <elzyproot@protonmail.com>
2026-02-16 07:56:48 +0700

    Update down_osint.py

--- app/utility/down_osint.py
+++ app/utility/down_osint.py
@@ -8,7 +8,7 @@ def install_osint_module():
     repo_url = "https://github.com/storm-os/OSINT.git"
     target_dir = os.path.join(ROOT, "script", "osint")
 
-    # 1. Clone/Update Repo
+    # 1. Clone Repo
     if os.path.exists(os.path.join(target_dir, ".git")):
         print("[!] Module found. Updating...")
         subprocess.run(["git", "-C", target_dir, "pull"], check=True)
@@ -16,11 +16,11 @@ def install_osint_module():
         print("[*] Downloading OSINT Module...")
         subprocess.run(["git", "clone", repo_url, target_dir], check=True)
 
-    # 2. Instalasi via setup.py
+    # 2. Installation via setup.py
     setup_file = os.path.join(target_dir, "setup.py")
     if os.path.exists(setup_file):
         print("[*] Detected setup.py. Installing module in editable mode...")
-        # '-e .' artinya install folder ini sebagai package agar bisa di-import
+        # '-e .' This means install this folder as a package so that it can be imported.
         subprocess.run(
             [sys.executable, "-m", "pip", "install", "-e", target_dir], check=True
         )
@@ -28,8 +28,10 @@ def install_osint_module():
 
     try:
         from scripts.security.sign import generate_folder_manifest
-
+        import app.utility.load_var as ld
+        
         generate_folder_manifest()
+        ld.load_var()
     except Exception as e:
         print(f"ERROR: {e}")
 
