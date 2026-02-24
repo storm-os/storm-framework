aa56daa9070ae7b5ce5982d6f0487e38db735d1a
Elzy🐧 <elzyproot@protonmail.com>
2026-02-25 02:47:48 +0700

    Add res folder before taking session

--- lib/smf/core/sf/ldch.py
+++ lib/smf/core/sf/ldch.py
@@ -9,7 +9,7 @@ from rootmap import ROOT
 
 
 def session(options):
-    full_path = os.path.join(ROOT, "lib", "smf", "cache")
+    full_path = os.path.join(ROOT, "lib", "smf", "cache", "res")
     cache_path = os.path.join(full_path, ".storm-session")
     # check cache files
     if os.path.exists(cache_path):
