5859011fdd3e05dd8638a03713479224748df19c
Elzy🐧 <218202059+storm-os@users.noreply.github.com>
2026-03-03 23:31:43 +0700

    Update versi.py

--- versi.py
+++ versi.py
@@ -1,7 +1,8 @@
 # MIT License.
 # Copyright (c) 2026 Storm Framework
-
 # See LICENSE file in the project root for full license information.
 
-
-VERSION = "3.5.10"
+# https://github.com/STORM-CORP/version.txt
+VERSION = "3.5.11"
+# Change when doing a major update to the storm function
+# 
